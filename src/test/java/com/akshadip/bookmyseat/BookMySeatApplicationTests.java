612302c926a7987630c0978ded67fce69b356d9a
unknown <simplewisdoms267@gmail.com>
2026-03-18 23:15:46 +0530

    add project structure and base files

---
+++ src/test/java/com/akshadip/bookmyseat/BookMySeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshadip.bookmyseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMySeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
